038ec92f9c3529b02031486f4ec9d1b30426ddd9
Niklas Melton <niklasmelton@gmail.com>
2026-07-17 09:23:30 -0500

    update formula rendering

--- docs/conf.py
+++ docs/conf.py
@@ -22,10 +22,16 @@ version = release
 
 extensions = [
     "myst_parser",
+    "sphinx.ext.mathjax",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
+
+# MyST leaves dollar-delimited math disabled unless this extension is enabled.
+# It applies to both inline ``$...$`` and display ``$$...$$`` expressions in
+# the Markdown documentation; Sphinx renders the resulting math with MathJax.
+myst_enable_extensions = ["dollarmath"]
 
 source_suffix = {
     ".rst": "restructuredtext",
